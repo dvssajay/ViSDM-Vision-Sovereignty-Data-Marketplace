b929e2363daef9966461b4240de45078e41956a5
Venkata Satya Sai Ajay Daliparthi <dvssajay@gmail.com>
2023-06-07 14:56:06 +0200

    Add files via upload

---
+++ Offchain/ModelTraining/exp1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 flower_main1.py > node1.txt
+
+python3 flower_main2.py > node2.txt
+
+python3 flower_main3.py > node3.txt
+
+python3 flower_main4.py > node4.txt
+   
+python3 flower_main5.py > node5.txt
---
+++ Offchain/ModelTraining/exp2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+python3 flower_main2.py > node2.txt
+   
+python3 flower_main5.py > node5.txt
+
+python3 flower_main25.py > node25.txt
+
+python3 flower_main50.py > node50.txt
+
